d4fce84286b6d2dc9e56e17fe7c0efd566ea8952
clark-cui <cuirongchuan@163.com>
2023-01-14 22:45:20 +0800

    feat: analyze for build

--- vite.config.ts
+++ vite.config.ts
@@ -2,34 +2,37 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import autoprefixer from "autoprefixer";
 import UnoCSS from "unocss/vite";
-
+import { visualizer } from "rollup-plugin-visualizer";
 // import path from 'path'
 
-export default defineConfig({
-  // root: path.join(__dirname, 'src'),
-  plugins: [
-    react(),
-    UnoCSS({
-      /* options */
-    }),
-  ],
-  css: {
-    modules: {
-      generateScopedName: "[name]__[local]___[hash:base64:5]",
+export default defineConfig(({ mode }) => {
+  return {
+    // root: path.join(__dirname, 'src'),
+    plugins: [
+      react(),
+      UnoCSS({
+        /* options */
+      }),
+      mode === "analyze" ? visualizer({ open: true, gzipSize: true }) : [],
+    ],
+    css: {
+      // css modules
+      modules: {
+        generateScopedName: "[name]__[local]___[hash:base64:5]",
+      },
+      postcss: {
+        plugins: [
+          autoprefixer({
+            overrideBrowserslist: [
+              "Chrome >= 45",
+              "Safari >= 10",
+              "iOS >= 10",
+              "Firefox >= 40",
+              "Edge >= 15",
+            ],
+          }),
+        ],
+      },
     },
-    postcss: {
-      plugins: [
-        autoprefixer({
-          // 指定目标浏览器
-          overrideBrowserslist: [
-            "Chrome >= 45",
-            "Safari >= 10",
-            "iOS >= 10",
-            "Firefox >= 40",
-            "Edge >= 15",
-          ],
-        }),
-      ],
-    },
-  },
+  };
 });
