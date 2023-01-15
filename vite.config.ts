752afd84c21bd9c5efaad26c64b45e9755e83ecf
clark-cui <cuirongchuan@163.com>
2023-01-15 22:10:01 +0800

    refector: build performance

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import autoprefixer from 'autoprefixer';
 import UnoCSS from 'unocss/vite';
 import { visualizer } from 'rollup-plugin-visualizer';
 import path from 'path';
-import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
-import legacy from '@vitejs/plugin-legacy';
+// chunksplit 在vite4有bug，先不用
+// import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
+// legacy先默认不开启，如果有需要再打开
+// import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig(({ mode }) => {
   return {
@@ -33,10 +35,9 @@ export default defineConfig(({ mode }) => {
         /* options */
       }),
       // 分析打包
-      mode === 'analyze' ? visualizer({ open: true, gzipSize: true }) : [],
-      // // // 分离资源
+      mode === 'analyze' ? visualizer({ open: true, gzipSize: true }) : []
+      // // split chunk
       // chunkSplitPlugin({
-      //   // 指定拆包策略
       //   customSplitting: {
       //     // 1. 支持填包名。`react` 和 `react-dom` 会被打包到一个名为`react-vendor`的 chunk 里
       //     'react-vendor': ['react', 'react-dom'],
@@ -46,16 +47,16 @@ export default defineConfig(({ mode }) => {
       //     // 'components-util': [/src\/components/, /src\/utils/]
       //   }
       // })
-      // 设置js降级处理的目标浏览器
-      legacy({
-        targets: [
-          'Chrome >= 45',
-          'Safari >= 10',
-          'iOS >= 10',
-          'Firefox >= 40',
-          'Edge >= 15'
-        ]
-      })
+      // // 设置js降级处理的目标浏览器
+      // legacy({
+      //   targets: [
+      //     'Chrome >= 45',
+      //     'Safari >= 10',
+      //     'iOS >= 10',
+      //     'Firefox >= 40',
+      //     'Edge >= 15'
+      //   ]
+      // })
     ],
     css: {
       // css modules
@@ -82,19 +83,13 @@ export default defineConfig(({ mode }) => {
       stringify: true
     },
     build: {
-      // 压缩代码配置。类型: boolean | 'esbuild' | 'terser'
-      // 默认为 `esbuild`
+      // 压缩代码配置。 boolean | 'esbuild' | 'terser'
       minify: 'esbuild',
-      // 产物目标环境
-      // 默认是modules，也就是['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1']
-      // exnext 是最新语法
-      // es2015/es6是比较稳妥的
+      // 产物目标环。 modules、exnext、es2015/es6
       target: 'es2015',
       // 如果 minify 为 terser，可以通过下面的参数配置具体行为
-      // https://terser.org/docs/api-reference#minify-options
-      terserOptions: {},
-      // assets是处理base64还是分离
-      // >=5 KB是单独文件，否则是base64
+      // terserOptions: {},
+      // assets是处理base64还是分离。 >=5 KB是单独文件，否则是base64
       assetsInlineLimit: 5 * 1024
     }
   };
