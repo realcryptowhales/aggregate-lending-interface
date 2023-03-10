64c9641c9ef3287e1855861228b717c987bfe53a
yunlong.sun <yunlong.sun@okg.com>
2023-03-10 11:00:03 +0800

    update

--- src/containers/Porfolio/AssetInfo/index.tsx
+++ src/containers/Porfolio/AssetInfo/index.tsx
@@ -1,7 +1,7 @@
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import style from './index.module.less';
 import cls from 'classnames';
-import { Tooltip } from '@mui/material';
+import { Button, Tooltip } from '@mui/material';
 import { useStore } from '@/stores';
 import {
   formatPercent,
@@ -9,6 +9,8 @@ import {
   thousandNumber
 } from '@/utils/format';
 import { observer } from 'mobx-react-lite';
+import SmallDialog from '@/components/SmallDialog';
+import BigNumber from 'bignumber.js';
 // interface Props {
 // title: React.ReactNode;
 // secondTitle?: string;
@@ -22,7 +24,7 @@ const InfoItem: React.FC<{
   hasTips?: boolean;
 }> = ({ title, amount, percentText, percent, hasTips }) => {
   return (
-    <div style={{ marginRight: 59, minWidth: 180 }}>
+    <div style={{ minWidth: 180 }}>
       <div className={style.title}>{title}</div>
       <div className={style.amount}>{amount}</div>
       {percentText && (
@@ -56,7 +58,10 @@ const AssetInfo: React.FC = ({}) => {
       netProfit,
       collateralValue,
       usedRatio,
-      borrowLimit
+      borrowLimit,
+      totalAvailableBorrow,
+      curLtv,
+      liquidateThreashold
     }
   } = useStore();
   const thousandCollateralValue = useMemo(() => {
@@ -66,68 +71,145 @@ const AssetInfo: React.FC = ({}) => {
   const percentUsedRatio = useMemo(() => {
     return usedRatio ? formatPercent(usedRatio) : '--';
   }, [usedRatio]);
+  const percentCltv = useMemo(() => {
+    return curLtv ? formatPercent(curLtv) : '--';
+  }, [curLtv]);
   const thousandBorrowLimit = useMemo(() => {
     if (Number.isNaN(+borrowLimit) || !borrowLimit) return '--';
     return thousandCurrency(borrowLimit);
   }, [borrowLimit]);
   console.log('usedRatio', usedRatio);
   console.log('netProfit', netProfit);
+
+  const [liquidationModal, setLiquidationModal] = useState(false);
+  const [hasClosedModal, setHasClosedModal] = useState(false);
+  useEffect(() => {
+    const v = new BigNumber(liquidateThreashold)
+      .minus(new BigNumber(curLtv))
+      .lte(0.1);
+    console.log('hasClosedModal', hasClosedModal);
+    !hasClosedModal && setLiquidationModal(v);
+  }, [liquidateThreashold, curLtv, hasClosedModal]);
   return (
-    <div className={cls(style.container)}>
-      <InfoItem
-        title={'总存款金额'}
-        amount={userTotalSupplied ? thousandCurrency(userTotalSupplied) : '--'}
-        percentText={'总存款APR'}
-        percent={totalSuppliedApr ? formatPercent(totalSuppliedApr) : '--'}
-      />
-      <InfoItem
-        title={'总借款金额'}
-        amount={userTotalBorrowed ? thousandCurrency(userTotalBorrowed) : '--'}
-        percentText={'总借款APR'}
-        percent={totalBorrowedApr ? formatPercent(totalBorrowedApr) : '--'}
-      />
-      <InfoItem
-        title={'今日预估总收益'}
-        amount={dailyEstProfit ? thousandCurrency(dailyEstProfit) : '--'}
-        percentText="净收益率"
-        percent={netProfit ? formatPercent(netProfit) : '--'}
-        hasTips
-      />
-      <InfoItem title={'总抵押品价值'} amount={thousandCollateralValue} />
-      <div className={style.limit}>
-        <div className={style.title}>借款限额</div>
-        <div className={cls('flex justify-between', style.percent)}>
-          <span>已用比例</span>
-          <span>{percentUsedRatio}</span>
+    <>
+      <div className={cls(style.container)}>
+        <InfoItem
+          title={'总存款金额'}
+          amount={
+            userTotalSupplied ? thousandCurrency(userTotalSupplied) : '--'
+          }
+          percentText={'总存款APR'}
+          percent={totalSuppliedApr ? formatPercent(totalSuppliedApr) : '--'}
+        />
+        <InfoItem
+          title={'总借款金额'}
+          amount={
+            userTotalBorrowed ? thousandCurrency(userTotalBorrowed) : '--'
+          }
+          percentText={'总借款APR'}
+          percent={totalBorrowedApr ? formatPercent(totalBorrowedApr) : '--'}
+        />
+        <InfoItem
+          title={'今日预估总收益'}
+          amount={dailyEstProfit ? thousandCurrency(dailyEstProfit) : '--'}
+          percentText="净收益率"
+          percent={netProfit ? formatPercent(netProfit) : '--'}
+          hasTips
+        />
+        <InfoItem title={'总抵押品价值'} amount={thousandCollateralValue} />
+        <div className={style.limit}>
+          <div className={cls('flex justify-between', style.font14)}>
+            <span>借款额度</span>
+            <span>
+              {totalAvailableBorrow
+                ? thousandCurrency(totalAvailableBorrow)
+                : '--'}
+            </span>
+          </div>
+          <div className={cls('flex justify-between', style.font14)}>
+            <span>剩余可借额度</span>
+            <span>{thousandBorrowLimit}</span>
+          </div>
         </div>
-        <div
-          style={{
-            backgroundColor: '#EBEBEB',
-            height: 4,
-            position: 'relative',
-            margin: '6px 0px',
-            borderRadius: '2px'
-          }}
-        >
-          <div
-            style={{
-              backgroundColor: '#424242',
-              width: `${+usedRatio * 100}%`,
-              height: 4,
-              borderRadius: '2px'
-            }}
-          ></div>
-          <div
-            className={style.arrow}
-            style={{ left: `calc(${+usedRatio * 100}% - 3px)`, top: '-6px' }}
-          ></div>
-        </div>
-        <div className={cls('flex justify-between', style.percent)}>
-          <span>最多可借</span>
-          <span>{thousandBorrowLimit}</span>
+        <div className={style.wrap}>
+          <div className={cls('flex justify-between', style.font14)}>
+            <span>当前抵押率</span>
+            <span>{percentCltv}</span>
+          </div>
+          <div>
+            <div
+              style={{
+                backgroundColor: '#EBEBEB',
+                height: 4,
+                position: 'relative',
+                margin: '6px 0px',
+                borderRadius: '2px'
+              }}
+            >
+              <div
+                style={{
+                  backgroundColor: '#424242',
+                  width: `${+curLtv * 100}%`,
+
+                  height: 4,
+                  borderRadius: '2px'
+                }}
+              ></div>
+              <div
+                className={style.arrow}
+                style={{ left: `calc(${+curLtv * 100}% - 3px)`, top: '-6px' }}
+              ></div>
+              <div
+                className={style.liquidation}
+                style={{
+                  left: `calc(${+liquidateThreashold * 100}% - 3px)`
+                }}
+              ></div>
+            </div>
+            <div className={cls('flex justify-between', style.font14)}>
+              <span color="#dd1f16">Liauidation</span>
+              <span>
+                {liquidateThreashold
+                  ? formatPercent(liquidateThreashold)
+                  : '--'}
+              </span>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+      <SmallDialog
+        open={liquidationModal}
+        title="清算提醒"
+        content={
+          <div className="flex flex-col items-center justify-center min-w-85.75 min-h-47 text-3.5 leading-4">
+            你当前的仓位抵押率过高，即将面临清算，请增加抵押品或者偿还部分借款。
+          </div>
+        }
+        button={
+          <Button
+            style={{
+              width: '130px',
+              height: '40px'
+            }}
+            sx={{
+              background: '#000000',
+              color: '#ffffff',
+              '&:hover': { background: '#000000' }
+            }}
+            onClick={() => {
+              setLiquidationModal(false);
+              setHasClosedModal(true);
+            }}
+          >
+            我已知晓
+          </Button>
+        }
+        handleClose={() => {
+          setLiquidationModal(false);
+          setHasClosedModal(true);
+        }}
+      ></SmallDialog>
+    </>
   );
 };
 
